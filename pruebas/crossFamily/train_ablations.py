46b64bd4a463618e4e64327dc40f85ceee58d9bc
Gisch97 <gkulemeyer@gmail.com>
2024-11-05 04:06:02 +0000

    noResnet1d

--- pruebas/crossFamily/train_ablations.py
+++ pruebas/crossFamily/train_ablations.py
@@ -20,42 +20,41 @@ from sincfold.ablation.ablation_train import train_net
 
 # family = 'RNaseP'
 
-ABLATIONS = 'sincfold'
+ABLATION = 'sincfold'
 valid_with_test=True
 
-for ABLATION in ABLATIONS:
+########################################
+# Paths definitions
+
+CROSS_FAMILIES_PATH = 'data/'   
+CROSS_FAMILIES_WEIGHTS = 'weights/'
+TRAIN_LOG = 'train_log.csv'
+
+families = ['tRNA', 'telomerase', 'RNaseP']
+TRAIN_FAMILY = {fam: CROSS_FAMILIES_PATH + 'train_' + fam + '.csv' for fam in families}
+TEST_FAMILY = {fam: CROSS_FAMILIES_PATH + 'test_' + fam + '.csv' for fam in families}
+
+# F = CROSS_FAMILIES_WEIGHTS + family +'/'+ABLATION
+########################################
+for family in families:
+    # Args    
+    TRAIN_FILE = TRAIN_FAMILY[family]
+    OUT_PATH = CROSS_FAMILIES_WEIGHTS + family + '/' + ABLATION
+    VALID_FILE=None
+    if valid_with_test:
+        OUT_PATH+= '/valid_with_test/'
+        VALID_FILE= TEST_FAMILY[family]
+        
+    print(f'''
+        ABLATION: {ABLATION}, \n
+        TRAIN_FILE: {TRAIN_FILE}, \n
+        OUT_PATH: {OUT_PATH}, \n
+        VALID_FILE: {VALID_FILE}, \n
+        ''')
     ########################################
-    # Paths definitions
-
-    CROSS_FAMILIES_PATH = 'data/'   
-    CROSS_FAMILIES_WEIGHTS = 'weights/'
-    TRAIN_LOG = 'train_log.csv'
-
-    families = ['tRNA', 'telomerase', 'RNaseP']
-    TRAIN_FAMILY = {fam: CROSS_FAMILIES_PATH + 'train_' + fam + '.csv' for fam in families}
-    TEST_FAMILY = {fam: CROSS_FAMILIES_PATH + 'test_' + fam + '.csv' for fam in families}
-
-    # F = CROSS_FAMILIES_WEIGHTS + family +'/'+ABLATION
-    ########################################
-    for family in families:
-        # Args    
-        TRAIN_FILE = TRAIN_FAMILY[family]
-        OUT_PATH = CROSS_FAMILIES_WEIGHTS + family + '/' + ABLATION
-        VALID_FILE=None
-        if valid_with_test:
-            OUT_PATH+= '/valid_with_test/'
-            VALID_FILE= TEST_FAMILY[family]
-            
-        print(f'''
-            ABLATION: {ABLATION}, \n
-            TRAIN_FILE: {TRAIN_FILE}, \n
-            OUT_PATH: {OUT_PATH}, \n
-            VALID_FILE: {VALID_FILE}, \n
-            ''')
-        ########################################
-        # # Execution
-        train_net(
-        ablation= ABLATION,
-            train_file= TRAIN_FILE,
-            out_path= OUT_PATH,
-            valid_file= VALID_FILE)
+    # # Execution
+    train_net(
+    ablation= ABLATION,
+        train_file= TRAIN_FILE,
+        out_path= OUT_PATH,
+        valid_file= VALID_FILE)
