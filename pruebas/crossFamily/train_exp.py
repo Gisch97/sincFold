e25047965a7d7342c53e201526ee14bc4f8f3db9
Gisch97 <gkulemeyer@gmail.com>
2024-11-07 20:27:27 +0000

    agrego experimento errores

---
+++ pruebas/crossFamily/train_exp.py
@@ -0,0 +1,59 @@
+import os 
+from sincfold.ablation.ablation_train import train_net
+
+    ########################################
+    # family:                              #
+    #     'tRNA'                           #
+    #     'telomerase'                     #
+    #     'RNaseP'                         #
+    #                                      #
+    # ablations:                           #
+    #     'sincfold'                       #
+    #     'C1D_C2D'                        #
+    #     'no_ResNet2d'                    #
+    #     '1ResNet2d'                      #
+    #     'no_ResNet1d'                    #
+    #     'no_ResNet1d_FF'                 #
+    ########################################
+
+# Calculation
+
+ABLATION = 'no_ResNet2d'                    
+valid_with_test=True
+
+########################################
+# Paths definitions
+   
+CROSS_FAMILIES_WEIGHTS = 'exp-diff-errores/'
+TRAIN_LOG = 'train_log.csv'
+
+families = 'telomerase'
+
+TRAIN_FAMILY = 'data/train_telomerase.csv'
+TEST_FAMILY = 'data/test_telomerase.csv'
+# F = CROSS_FAMILIES_WEIGHTS + family +'/'+ABLATION
+########################################
+# for family in families:
+# Args    
+TRAIN_FILE = TRAIN_FAMILY
+OUT_PATH = CROSS_FAMILIES_WEIGHTS + 'valid_none'
+# VALID_FILE=None
+if valid_with_test:
+    OUT_PATH+= '/valid_with_test/'
+    VALID_FILE= TEST_FAMILY
+    
+print(f'''
+    ABLATION: {ABLATION}, \n
+    TRAIN_FILE: {TRAIN_FILE}, \n
+    OUT_PATH: {OUT_PATH}, \n
+    VALID_FILE: {VALID_FILE}, \n
+    NUM EPOCH: 5, \n
+    ''')
+    ########################################
+    # # Execution
+train_net(
+ablation= ABLATION,
+    train_file= TRAIN_FILE,
+    out_path= OUT_PATH,
+    valid_file= VALID_FILE,
+    num_epoch=5)
